9e617a36e63efae6910afff4a691ae5725fc3e94
FiHammer <lualbi11@aol.de>
2019-07-16 23:31:39 +0200

    Evecon push to 0.9.9.1 (to use download EveconLib)

--- Klakum.py
+++ Klakum.py
@@ -1,6 +1,6 @@
 from KlakumLib import *
 
-
+import EveconLib
 
 GPIO.setmode(GPIO.BCM)
 
@@ -8,7 +8,7 @@ relay_list = [Relay(5), Relay(25), Relay(24), Relay(23), Relay(22), Relay(27), R
 relay_surge_list = [RelaySurge(17)]
 
 
-def reacter(msg):
+def reacter(msg, conId):
     msg_split = msg.split("_")
 
     if msg_split[0] == "relay":
@@ -19,7 +19,7 @@ def reacter(msg):
             relay_list[relay_id].set(msg_split[3])
 
         elif msg_split[2] == "get":
-            Klakum_Server.send("relay_" + str(relay_id) + "_return_" + str(relay_list[relay_id].get()))
+            Klakum_Server.sendToID("relay_" + str(relay_id) + "_return_" + str(relay_list[relay_id].get()), conId)
 
         elif msg_split[2] == "switch":
             relay_list[relay_id].switch()
@@ -32,8 +32,14 @@ def reacter(msg):
             relay_surge_list[relay_id].switch()
 
 
-Klakum_Server = Server(1007, reacter, ip="192.168.2.107", BigServerBuffersize=0)
+
+
+#Klakum_Global_Server = Server(10007, reacter, ip="79.240.185.228", BigServerBuffersize=0)
+#Klakum_Global_Server.start()
+
+Klakum_Server = EveconLib.Networking.Server(1007, reacter, ip="192.168.2.107", secuLevel=-1)
 Klakum_Server.start()
+#Klakum_Global_Server.join()
 Klakum_Server.join()
 
 GPIO.cleanup()
